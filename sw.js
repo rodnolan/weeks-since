eb4c1b0f279ebb3a9b3c6710ee2fcd42665561f8
Rod Nolan <rodnolan@gmail.com>
2026-08-30 20:57:27 -0400

    update the pre-commit hook to also display the version on the page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v0.0.1'; // The script will automatically edit this string
+const CACHE_NAME = 'app-cache-v0.0.2'; // The script will automatically edit this string
 const urlsToCache = [
   'index.html',
   'style.css',
