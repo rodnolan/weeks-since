fca62b15156e10c6f72f03a5a7e31af22dee7fd1
Rod Nolan <rodnolan@gmail.com>
2026-09-13 09:09:46 -0400

    refactor service worker registration script into a separate file

---
+++ sw-register.js
@@ -0,0 +1,8 @@
+// register the service worker script to enable offline capabilities
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(reg => console.log('Service Worker registered!', reg))
+            .catch(err => console.log('Registration failed:', err));
+    });
+}
